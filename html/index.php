ce8815d94917f959fbc454561ca8a9004e01057a
cht.andy <cht.andy@gmail.com>
2021-06-26 15:39:36 +0000

    update

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <?php
 $ip = $_SERVER['SERVER_ADDR'];
-echo "IP ADDR is: $ip", "<br>";
-echo "PHP Version: " . phpversion(). "<br>";
+echo "<b>Container IP is: $ip", "<br>";
+echo "<b>PHP Version: " . phpversion(). "<br>";
 $DB_HOPST = (getenv('DBHOST')?:'192.168.0.1');
-echo "<b> Mariadb Server Host: </b>" . (getenv('DBHOST')?:'192.168.0.1');
+echo "<b> DB Server Host: </b>" . (getenv('DBHOST')?:'192.168.0.1');
 ?>
